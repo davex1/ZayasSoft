80e01b2763e4ec00aff37a8faffbe836a134b6fa
davex <davexdaw@gmail.com>
2015-04-23 08:48:02 +0200

    V 1.1

--- Zayasoft/src/Modulo1.java
+++ Zayasoft/src/Modulo1.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 
 public class Modulo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n;
+		int i = 0;
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Introduce un numero:");
+		n=teclado.nextInt();
+		
+		while(i<n){
+			System.out.println("Modulo ejecutándose");
+			i++;
+		}
+		
+	}
+	
+	
 
 }
